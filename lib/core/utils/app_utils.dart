f32cb2331bf3cd73bd3e1abeb89d49604e817d1d
Vicviral <lovedayv7@gmail.com>
2024-03-12 01:06:38 +0100

    form validators

--- lib/core/utils/app_utils.dart
+++ lib/core/utils/app_utils.dart
@@ -1,3 +1,5 @@
+import 'package:form_field_validator/form_field_validator.dart';
+
 String shortenString(String input, int maxLength) {
   if (input.length <= maxLength) {
     return input;
@@ -11,3 +13,28 @@ String shortenStringWithReadMore(String input, int maxLength) {
   }
   return '${input.substring(0, maxLength)}...Read more';
 }
+
+final passwordValidator = MultiValidator([
+  RequiredValidator(errorText: 'Required*'),
+  MinLengthValidator(8, errorText: 'Password must be at least 8 digits long'),
+  PatternValidator(r'(?=.*?[0-9])', errorText: 'Password must contain at least one number'),
+  PatternValidator(r'(?=.*?[A-Z])', errorText: 'Password must contain at least one uppercase letter'),
+  PatternValidator(r'(?=.*?[a-z])', errorText: 'Password must contain at least one lowercase letter'),
+  PatternValidator(r'(?=.*?[#?!@$%^&*-])', errorText: 'Passwords must have at least one special character')
+]);
+
+final emailValidator = MultiValidator([
+  RequiredValidator(errorText: 'Required*'),
+  EmailValidator(errorText: "Invalid email"),
+]);
+
+final requiredValidator = RequiredValidator(errorText: 'Required*');
+
+String? matchPasswordValidator(String? password, String? confirmPassword) {
+  if (password != confirmPassword) {
+    return 'Passwords do not match';
+  }
+  return null;
+}
+
+

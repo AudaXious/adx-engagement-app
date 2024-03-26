b13f659433503b5e357f1a0b18e496a1fb0b24da
Vicviral <lovedayv7@gmail.com>
2024-03-26 13:06:36 +0100

    get campaigns by space id usecase

---
+++ lib/domain/usecases/spaces/campaigns_by_space_id_usecase.dart
@@ -0,0 +1,21 @@
+import 'package:audaxious/data/repository/spaces_repository_impl.dart';
+import 'package:audaxious/domain/repository/spaces_repository.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+abstract class CampaignsBySpaceIdUseCase {
+  Future<dynamic> getCampaignsBySpaceId(String spaceId);
+
+}
+class CampaignsBySpaceIdUseCaseImpl extends CampaignsBySpaceIdUseCase {
+  final SpacesRepository spacesRepository;
+
+  CampaignsBySpaceIdUseCaseImpl(this.spacesRepository);
+
+  @override
+  Future<dynamic> getCampaignsBySpaceId(String spaceId) async {
+    return await spacesRepository.getCampaignsBySpaceId(spaceId);
+  }
+}
+
+final spaceDetailsUseCaseProvider = Provider<CampaignsBySpaceIdUseCase>((ref) {
+  return CampaignsBySpaceIdUseCaseImpl(ref.read(spacesRepositoryProvider));
+});

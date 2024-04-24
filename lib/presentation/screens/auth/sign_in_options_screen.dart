c816767932bac5d6e5e417cc97f401cf8d5f4886
Vicviral <lovedayv7@gmail.com>
2024-04-24 11:05:03 +0100

    bug fix

--- lib/presentation/screens/auth/sign_in_options_screen.dart
+++ lib/presentation/screens/auth/sign_in_options_screen.dart
@@ -7,7 +7,6 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:toastification/toastification.dart';
 import 'package:web3modal_flutter/services/w3m_service/i_w3m_service.dart';
 import 'package:web3modal_flutter/web3modal_flutter.dart';
-
 import '../../../core/routes/app_router.dart';
 import '../../../core/utils/app_layout.dart';
 import '../../../core/utils/theme/dark_theme.dart';
@@ -17,44 +16,75 @@ import '../../widgets/alerts/custom_toast.dart';
 import '../../widgets/buttons/primary_button.dart';
 import '../../widgets/buttons/secondary_button.dart';
 @RoutePage()
-class SignInOptionsScreen extends HookConsumerWidget {
+class SignInOptionsScreen extends HookConsumerWidget{
   SignInOptionsScreen({super.key});
 
   late W3MService _w3mService;
 
-  void initializeW3MService() async {
-    print("Initialization started");
-
-    _w3mService = W3MService(
-      projectId: '70630571fe8c62c8b5dfffe48ebc8c79',
-      metadata: const PairingMetadata(
-        name: 'Audaxious',
-        description: 'Connect wallet to AudaXious',
-        url: 'https://www.audaxious.com/',
-        icons: ['https://walletconnect.com/walletconnect-logo.png'],
-        redirect: Redirect(
-          native: 'audaxious://',
-          universal: 'https://www.walletconnect.com',
-        ),
-      ),
-    );
-
-    await _w3mService.init();
-    // if (_w3mService.status == W3MServiceStatus.initialized) {
-    //   print("The fucking service has been initialised");
-    //   // loginUser();
-    // }
-
-  }
-
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final size = AppLayout.getSize(context);
-    // final reader = ref.read(WalletLoginViewModel.notifier.notifier);
+    final reader = ref.read(WalletLoginViewModel.notifier.notifier);
     final notifier = ref.read(WalletLoginViewModel.notifier);
 
+    void initializeW3MService() async {
+      print("Initialization started");
+
+      _w3mService = W3MService(
+        projectId: '70630571fe8c62c8b5dfffe48ebc8c79',
+        metadata: const PairingMetadata(
+          name: 'Audaxious',
+          description: 'Connect wallet to AudaXious',
+          url: 'https://www.audaxious.com/',
+          icons: ['https://walletconnect.com/walletconnect-logo.png'],
+          redirect: Redirect(
+            native: 'audaxious://',
+            universal: 'https://www.walletconnect.com',
+          ),
+        ),
+      );
+
+      await _w3mService.init();
+      if (_w3mService.status == W3MServiceStatus.initialized) {
+        print("Web 3 status ${_w3mService.status}");
+        print("Web 3 address ${_w3mService.session?.address}");
+        // loginUser();
+      }
+
+    }
+
+    void loginUser() async {
+      print("Login started");
+      final walletId = _w3mService.session?.address;
+
+      if (walletId != null) {
+        final user = await reader.loginUser(walletId);
+        if (user == null) {
+          if (!context.mounted) return;
+          CustomToast.show(
+            context: context,
+            title: "Error",
+            description: "Failed to login user. Please try again!",
+            type: ToastificationType.error,
+          );
+
+        }else {
+          if (user.username == null) {
+            if (!context.mounted) return;
+            // context.router.navigate(CreateUsernameRoute());
+          }else {
+            if (!context.mounted) return;
+            // context.router.replaceAll([const BottomBarRoute()]);
+          }
+        }
+
+      }else {
+      }
+
+    }
+
     useEffect(() {
-      initializeW3MService();
+      // initializeW3MService();
       return () {
       };
     }, const []);
@@ -150,83 +180,83 @@ class SignInOptionsScreen extends HookConsumerWidget {
                   ),
 
                   Positioned(
-                    top: 330,
-                    left: 0,
-                    right: 0,
-                    child: Column(
-                      children: [
-                        Text(
-                          "Sign In",
-                          style: Theme.of(context).textTheme.displayLarge,
-                        ),
-                        const Gap(20),
-                        Container(
-                            margin: const EdgeInsets.symmetric(horizontal: 10),
-                            child: PrimaryButton(
-                              buttonText: "Sign In with Email",
-                              onPressed: () {
-                                context.router.navigate(LoginRoute());
-                              },
-                            )
-                        ),
-                        const Gap(20),
-                        const Text("Or"),
-                        const Gap(20),
-                        Container(
-                            margin: const EdgeInsets.symmetric(horizontal: 10),
-                            child: SecondaryButton(
-                              buttonText: "Sign In with Google",
-                              icon: "assets/images/google.png",
-                              onPressed: () {
-                              },
-                            )
-                        ),
-                        const Gap(20),
-                        Container(
-                            margin: const EdgeInsets.symmetric(horizontal: 10),
-                            child: SecondaryButton(
-                              buttonText: "Sign In with Wallet",
-                              icon: "assets/images/wallet_connect.png",
-                              onPressed: () async {
-                                // context.router.navigate(WalletLoginRoute());
-                                // initializeW3MService();
-                                _w3mService.openModal(context);
-                              },
-                              buttonState: notifier.viewState.isLoading
-                                  ? ButtonState.loading
-                                  : ButtonState.active
-                            )
-                        ),
-                        // W3MConnectWalletButton(
-                        //   service: _w3mService,
-                        //   state: ConnectButtonState.idle,
-                        //   size: BaseButtonSize.regular,
-                        // ),
-                        const Gap(30),
-                        Container(
-                          margin: const EdgeInsets.symmetric(horizontal: 20),
-                          child: TextButton(
-                            onPressed: () {},
-                            child: RichText(
-                              text: TextSpan(
-                                text: 'By continuing, you agree our ',
-                                style: Theme.of(context).textTheme.displaySmall?.copyWith(color: greyTextColor, fontSize: 12),
-                                children: <TextSpan>[
-                                  TextSpan(
-                                    text: 'Terms & Conditions',
-                                    style: TextStyle(
-                                      color: secondaryColor,
-                                      fontSize: 12,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            )
-                            ,
+                      top: 330,
+                      left: 0,
+                      right: 0,
+                      child: Column(
+                        children: [
+                          Text(
+                            "Sign In",
+                            style: Theme.of(context).textTheme.displayLarge,
                           ),
-                        )
-                      ],
-                    )
+                          const Gap(20),
+                          Container(
+                              margin: const EdgeInsets.symmetric(horizontal: 10),
+                              child: PrimaryButton(
+                                buttonText: "Sign In with Email",
+                                onPressed: () {
+                                  context.router.navigate(LoginRoute());
+                                },
+                              )
+                          ),
+                          const Gap(20),
+                          const Text("Or"),
+                          const Gap(20),
+                          Container(
+                              margin: const EdgeInsets.symmetric(horizontal: 10),
+                              child: SecondaryButton(
+                                buttonText: "Sign In with Google",
+                                icon: "assets/images/google.png",
+                                onPressed: () {
+                                },
+                              )
+                          ),
+                          const Gap(20),
+                          Container(
+                              margin: const EdgeInsets.symmetric(horizontal: 10),
+                              child: SecondaryButton(
+                                  buttonText: "Sign In with Wallet",
+                                  icon: "assets/images/wallet_connect.png",
+                                  onPressed: () async {
+                                    // context.router.navigate(WalletLoginRoute());
+                                    // initializeW3MService();
+                                    _w3mService.openModal(context);
+                                  },
+                                  buttonState: notifier.viewState.isLoading
+                                      ? ButtonState.loading
+                                      : ButtonState.active
+                              )
+                          ),
+                          // W3MConnectWalletButton(
+                          //   service: _w3mService,
+                          //   state: ConnectButtonState.idle,
+                          //   size: BaseButtonSize.regular,
+                          // ),
+                          const Gap(30),
+                          Container(
+                            margin: const EdgeInsets.symmetric(horizontal: 20),
+                            child: TextButton(
+                              onPressed: () {},
+                              child: RichText(
+                                text: TextSpan(
+                                  text: 'By continuing, you agree our ',
+                                  style: Theme.of(context).textTheme.displaySmall?.copyWith(color: greyTextColor, fontSize: 12),
+                                  children: <TextSpan>[
+                                    TextSpan(
+                                      text: 'Terms & Conditions',
+                                      style: TextStyle(
+                                        color: secondaryColor,
+                                        fontSize: 12,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              )
+                              ,
+                            ),
+                          )
+                        ],
+                      )
                   )
                 ],
               ),
@@ -235,7 +265,8 @@ class SignInOptionsScreen extends HookConsumerWidget {
           ],
         ),
       ),
-    );
+    )
+    ;
   }
 }
 
@@ -250,46 +281,16 @@ class _AppLifecycleObserver extends WidgetsBindingObserver {
   @override
   void didChangeAppLifecycleState(AppLifecycleState state) {
     if (state == AppLifecycleState.resumed) {
-      signInOptionsScreen.initializeW3MService();
-      if (signInOptionsScreen._w3mService.status == W3MServiceStatus.initialized) {
-        print("The fucking service has been initialised");
-        loginUser();
-      }else {
-        print("The fucking service has not been initialised");
+      print("on resumed");
 
-      }
+      // signInOptionsScreen.initializeW3MService();
+      // print("Web 3 status ${signInOptionsScreen._w3mService.status}");
+      // print("Web 3 address ${signInOptionsScreen._w3mService.session?.address}");
+      // if (signInOptionsScreen._w3mService.status == W3MServiceStatus.initialized) {
+      //   loginUser();
+      // }else {
+      // }
     }
-  }
-
-  void loginUser() async {
-    print("Login started");
-    final reader = ref.read(WalletLoginViewModel.notifier.notifier);
-    final walletId = signInOptionsScreen._w3mService.session?.address;
-
-    if (walletId != null) {
-      final user = await reader.loginUser(walletId);
-      if (user == null) {
-        if (!context.mounted) return;
-        CustomToast.show(
-          context: context,
-          title: "Error",
-          description: "Failed to login user. Please try again!",
-          type: ToastificationType.error,
-        );
-
-      }else {
-        if (user.username == null) {
-          if (!context.mounted) return;
-          // context.router.navigate(CreateUsernameRoute());
-        }else {
-          if (!context.mounted) return;
-          // context.router.replaceAll([const BottomBarRoute()]);
-        }
-      }
-
-    }else {
-    }
-
   }
 
 }

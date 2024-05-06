5c5d7218052a9e4d59b99b9a618b78402cba7a52
Vicviral <lovedayv7@gmail.com>
2024-05-06 20:17:44 +0100

    Prevent the request retry mechanism from continuing indefinitely

--- lib/core/services/internet_services/network_connectivity.dart
+++ lib/core/services/internet_services/network_connectivity.dart
@@ -6,20 +6,24 @@ import 'package:dio/dio.dart';
 class NetworkConnectivityRequestRetrier {
   final Dio dio;
   final Connectivity connectivity;
+  final Duration retryTimeout;
 
   NetworkConnectivityRequestRetrier({
     required this.dio,
     required this.connectivity,
+    this.retryTimeout = const Duration(seconds: 60)
   });
 
   Future<dynamic> scheduleRequestRetry(RequestOptions requestOptions) async {
     late StreamSubscription streamSubscription;
     final responseCompleter = Completer<Response>();
+    bool retried = false;
 
     streamSubscription = connectivity.onConnectivityChanged.listen(
         (connectivityResults) {
           for (var connectivityResult in connectivityResults) {
-            if(connectivityResult != ConnectivityResult.none){
+            if(connectivityResult != ConnectivityResult.none && !retried){
+              retried = true;
               streamSubscription.cancel();
               responseCompleter.complete(
                   dio.request(
@@ -36,7 +40,14 @@ class NetworkConnectivityRequestRetrier {
         }
     );
 
-    return responseCompleter.future;
+    Future.delayed(retryTimeout, () {
+      if (!responseCompleter.isCompleted) {
+        retried = true;
+        streamSubscription.cancel();
+        responseCompleter.completeError('Retry timeout');
+      }
+    });
 
+    return responseCompleter.future;
   }
 }
